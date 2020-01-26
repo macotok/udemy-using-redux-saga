95b615f40fe9183db17b0e42205bc9ef1ac8c7b4
macotok <monochromer@r8.ucom.ne.jp>
2020-01-26 21:18:45 +0900

    firebaseでhosting

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './components/App';
 import reducers from './reducers';
 import rootSaga from './sagas';
+import firebase from 'firebase';
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+};
+firebase.initializeApp(config);
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
@@ -15,6 +22,6 @@ const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api'
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
